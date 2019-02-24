b5d970713b5aef38fbe5012a9b637bf493c6bdb2
Paritosh <p@Paritoshs-MacBook-Pro.local>
2019-02-23 20:07:07 -0500

    separating frontend branch

--- project-hub/website/backend/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    DB: 'mongodb://projecthub1:L03TY9pAulwt6t85yGoPNracwgXgJnWiIfHBKEndePbPGibBK5CZ0e2Y9qMpqiILWz8XHfSxO6hpOTUfXXvHbQ==@projecthub1.documents.azure.com:10255/Users?ssl=true&replicaSet=globaldb'
-}
