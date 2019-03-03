dff8a642e092775f32e8e4ca5404e5910039493c
Pulkit Mathur <mathurpulkit93@gmail.com>
2019-03-03 03:04:23 -0500

    modify test cases

--- feed-fetch-service/src/test/java/com/sga/microservice/feedfetchservice/FeedFetchServiceApplicationTests.java
+++ feed-fetch-service/src/test/java/com/sga/microservice/feedfetchservice/FeedFetchServiceApplicationTests.java
@@ -1,12 +1,17 @@
 package com.sga.microservice.feedfetchservice;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class FeedFetchServiceApplicationTests {
 
 	// private MockMvc mockMvc;
@@ -14,18 +19,32 @@ public class FeedFetchServiceApplicationTests {
 	// @MockBean
 	// private final MediaController mc = new MediaController();
 
+	@Autowired
+	MediaController mc;
+
+	@LocalServerPort
+	private int port;
+
 	@Test
 	public void contextLoads() {
 
+		final RestTemplate restTemplate = new RestTemplate();
+		// tweetRepository.save(tweet);
+		final String baseUrl = "http://localhost:" + port + "/getLinks";
+
+		final ResponseEntity<String> result = restTemplate.postForEntity(baseUrl, null, String.class);
+		System.out.println(result.getBody());
+		Assert.assertEquals(200, result.getStatusCodeValue());
+
 		/*
 		 * when(mc.getAllURLS()).thenReturn(toString());
-		 * 
+		 *
 		 * try { final MvcResult res = (MvcResult)
 		 * mockMvc.perform(MockMvcRequestBuilders.get("/getLinks"))
 		 * .andExpect(content().contentType(MediaType.APPLICATION_JSON));
 		 * System.out.println(res.getResponse()); } catch (final Exception e) { // TODO
 		 * Auto-generated catch block e.printStackTrace(); }
-		 * 
+		 *
 		 * verify(mc, times(1)).getAllURLS(); verifyNoMoreInteractions(mc);
 		 */
 	}
