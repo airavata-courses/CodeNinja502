9f8698d93b87c9c0a38a91df98aac85a78bfcb14
Pulkit Mathur <mathurpulkit93@gmail.com>
2019-03-28 19:10:34 -0400

    Update AppProperties.java

--- feed-fetch-service/src/main/java/com/sga/microservice/feedfetchservice/AppProperties.java
+++ feed-fetch-service/src/main/java/com/sga/microservice/feedfetchservice/AppProperties.java
@@ -2,9 +2,9 @@ package com.sga.microservice.feedfetchservice;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix="spring.cloud.consul.discovery")
+//@ConfigurationProperties(prefix="spring.cloud.consul.discovery")
 public class AppProperties {
-    private String ipAddress="127.4.5.6";
+    //private String ipAddress="127.4.5.6";
 
 }
 
