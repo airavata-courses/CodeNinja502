e16ad55468e88e8f101099d4e9645f81c20aca02
Pulkit Mathur <mathurpulkit93@gmail.com>
2019-03-30 17:45:29 -0400

    Update FeedFetchServiceApplication.java

--- feed-fetch-service/src/main/java/com/sga/microservice/feedfetchservice/FeedFetchServiceApplication.java
+++ feed-fetch-service/src/main/java/com/sga/microservice/feedfetchservice/FeedFetchServiceApplication.java
@@ -4,12 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-//import org.springframework.boot.context.properties.EnableConfigurationProperties;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableConfigurationProperties(AppProperties.class)
-@SpringBootApplication//(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
-//@EnableDiscoveryClient
+@SpringBootApplication
 public class FeedFetchServiceApplication {
 
 	public static void main(String[] args) {
