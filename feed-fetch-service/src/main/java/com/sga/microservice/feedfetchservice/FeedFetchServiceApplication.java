9a1a08b631b7101726606cceb122039815d1372c
Pulkit Mathur <mathurpulkit93@gmail.com>
2019-04-02 00:21:51 -0400

    Update FeedFetchServiceApplication.java

--- feed-fetch-service/src/main/java/com/sga/microservice/feedfetchservice/FeedFetchServiceApplication.java
+++ feed-fetch-service/src/main/java/com/sga/microservice/feedfetchservice/FeedFetchServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class FeedFetchServiceApplication {
 
 	public static void main(String[] args) {
