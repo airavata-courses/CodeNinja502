08ad9325af3abfa0c21e91839efe1b44a20817b0
Paritosh Morparia <33207301+morparia-p@users.noreply.github.com>
2019-04-26 10:28:40 -0400

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,3 @@
 module.exports = {
-    DB: 'mongodb://projecthub:Ywu2W9vK2xHSjj8SvZOCi5x4pc1gMmKrpjI9bU3xtPDE0UcOR8uRcyfvCeQBxyN6dKPfaBA0qjtJme0dux9mjA==@projecthub.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'
+    DB: 'mongodb://projecthub:Ywu2W9vK2xHSjj8SvZOCi5x4pc1gMmKrpjI9bU3xtPDE0UcOR8uRcyfvCeQBxyN6dKPfaBA0qjtJme0dux9mjA==@projecthub.documents.azure.com:10255/Users?ssl=true&replicaSet=globaldb'
 }
