b3a0320339e2abdecff2a5f3e2f687836d058b25
Pulkit Mathur <mathurpulkit93@gmail.com>
2019-04-25 21:12:02 -0400

    Update MediaRepository.java

--- feed-fetch-service/src/main/java/com/sga/microservice/feedfetchservice/MediaRepository.java
+++ feed-fetch-service/src/main/java/com/sga/microservice/feedfetchservice/MediaRepository.java
@@ -25,70 +25,14 @@ public class MediaRepository {
 	}
 
 	public String getURLS() {
-		/*inal String usernamepassword="http://127.0.0.1:8500/v1/kv/database/usernamepassword?raw";
-		final String sitename="http://127.0.0.1:8500/v1/kv/database/site?raw";
-		URL upass=null;
-		URL site=null;
-		String finalusernamepassword;
-		String finalsitename;
-		try {
-			upass = new URL(usernamepassword);
-			site = new URL(sitename);
-		}catch(Exception e){
-			System.out.println(e.getMessage());
-		}
-		StringBuilder result;
-		StringBuilder result2;
-		try {
-			HttpURLConnection conn = (HttpURLConnection) upass.openConnection();
-			conn.setRequestMethod("GET");
-			result = new StringBuilder();
-			BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String line;
-			while ((line = rd.readLine()) != null) {
-				result.append(line);
-			}
-
-
-			HttpURLConnection conn1 = (HttpURLConnection) site.openConnection();
-			conn1.setRequestMethod("GET");
-			result2 = new StringBuilder();
-			BufferedReader rd1 = new BufferedReader(new InputStreamReader(conn1.getInputStream()));
-			String line1;
-			while ((line1 = rd1.readLine()) != null) {
-				result.append(line1);
-			}
-		}catch (Exception e){
-			return (e.getMessage());
-		}
-
-
-		finalusernamepassword=result.toString();
-		finalsitename = result2.toString();
-		StringBuilder sb1 = new StringBuilder();
-
-		sb1.append("mongodb://");
-
-		sb1.append(finalusernamepassword);
-
-
-		sb1.append(finalsitename);
-
-		clientURI=sb1.toString();
-		//System.out.println("skjhfdkjhdskufhlkdsahfudhflkuhdflkuhlkdhuflkahflkashdflkahdflkhdflkj");
-		System.out.println(clientURI);*/
-
-		String res = null;
-
-		//final String clientURI = "mongodb://projecthub2:7CUAuyH3zX6RhgQLuUw8afiSkzuk5gKphGl9omu9DUb6nj2JTITkkhjpvf9oHFHUYABMNwHivWkQHfz4JNrLUw==@projecthub2.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
 		
-		
+		String res = null;		
 		
 		final String clientURI="mongodb://projecthub:Ywu2W9vK2xHSjj8SvZOCi5x4pc1gMmKrpjI9bU3xtPDE0UcOR8uRcyfvCeQBxyN6dKPfaBA0qjtJme0dux9mjA==@projecthub.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
 		final MongoClient client = new MongoClient(new MongoClientURI(clientURI));
 
-		final String dbName = "MediaDB";           // eElement.getElementsByTagName("dbName").item(0).getTextContent();
-		final String collectionName = "media";     // eElement.getElementsByTagName("collectionName").item(0).getTextContent();
+		final String dbName = "MediaDB";           
+		final String collectionName = "media";     
 		
 		@SuppressWarnings(value = { "deprecation" })
 		final DB db = client.getDB(dbName);
@@ -122,76 +66,6 @@ public class MediaRepository {
 		res = sb.toString();
 		client.close();
 		return res;
-
-		/*
-		 * String res = null; final String PATH = "login.xml"; final File fXmlFile = new
-		 * File(PATH); if (fXmlFile.exists()) { final DocumentBuilderFactory dbFactory =
-		 * DocumentBuilderFactory.newInstance(); DocumentBuilder dBuilder = null; try {
-		 * dBuilder = dbFactory.newDocumentBuilder(); } catch (final
-		 * ParserConfigurationException e) { e.printStackTrace(); } Document doc = null;
-		 * try { doc = dBuilder.parse(fXmlFile); } catch (final SAXException e) {
-		 * e.printStackTrace(); } catch (final IOException e) { e.printStackTrace(); }
-		 * doc.getDocumentElement().normalize();
-		 *
-		 * final NodeList nList = doc.getElementsByTagName("mediaDB"); final Node nNode
-		 * = nList.item(0);
-		 *
-		 * if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-		 *
-		 * final Element eElement = (Element) nNode; eElement.getAttribute("id");
-		 *
-		 * String clientURI = "mongodb://"; final String clientURI1 =
-		 * eElement.getElementsByTagName("clientURI1").item(0).getTextContent();
-		 *
-		 * final String clientURI2 =
-		 * eElement.getElementsByTagName("clientURI2").item(0).getTextContent();
-		 *
-		 * final String clientURI3 =
-		 * eElement.getElementsByTagName("clientURI3").item(0).getTextContent();
-		 *
-		 * final String clientURI4 =
-		 * eElement.getElementsByTagName("clientURI4").item(0).getTextContent();
-		 *
-		 * clientURI = clientURI + clientURI1 + "@" + clientURI2 + "ssl=" + clientURI3 +
-		 * "&" + clientURI4 + "=globaldb"; final MongoClient client = new
-		 * MongoClient(new MongoClientURI(clientURI));
-		 *
-		 * final String dbName =
-		 * eElement.getElementsByTagName("dbName").item(0).getTextContent(); final
-		 * String collectionName =
-		 * eElement.getElementsByTagName("collectionName").item(0).getTextContent();
-		 *
-		 * @SuppressWarnings(value = { "deprecation" }) final DB db =
-		 * client.getDB(dbName); final DBCollection collection =
-		 * db.getCollection(collectionName);
-		 *
-		 * final DBCursor iterDoc = collection.find(); final Iterator<?> it =
-		 * iterDoc.iterator();
-		 *
-		 * final StringBuilder sb = new StringBuilder(); sb.append("["); while
-		 * (it.hasNext()) { BasicDBObject obj; obj = (BasicDBObject) it.next();
-		 *
-		 * String a = obj.toJson(); final String datetemp =
-		 * StringUtils.substringBetween(a, "\"date\" : \"", "\""); final long epoch =
-		 * Long.parseLong(datetemp);
-		 *
-		 * final Calendar calendar = Calendar.getInstance();
-		 * calendar.setTimeInMillis(epoch);
-		 *
-		 * final PrettyTime p = new PrettyTime();
-		 *
-		 * a = StringUtils.replacePattern(a, "[1-9][0-9]{11,20}", p.format(calendar));
-		 *
-		 * sb.append(a); sb.append(","); }
-		 *
-		 * sb.setLength(sb.length() - 1); sb.append("]"); res = sb.toString();
-		 * client.close();
-		 *
-		 * } return res; }
-		 *
-		 * return "Login Credentials Not Found";
-		 */
-
 	}
 
 }
